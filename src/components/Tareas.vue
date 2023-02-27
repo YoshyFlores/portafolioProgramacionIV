78f0894045b2863bf728b91778b3a36f8bd7dfab
Yosselin Flores <yoshy.floresg17@gmail.com>
2023-02-26 23:15:50 -0600

    Portafolio

---
+++ src/components/Tareas.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="container text-center">
+
+    <br />
+    <div class="p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text-center">
+    <h4>Página web: CV y Asignación.</h4>
+    </div>
+    <br/>
+    <h5>En esta actividad se realizó la creación de un página web en la cual se plasmó una portada, nuestros curriculums, la realización de la asignación, nos correspondio crear un modal y de igual forma se agregaron conclusiones.</h5>
+    <br/>
+    
+<br/>
+    <img
+        src="../assets/tarea/aula.jpg"
+        class="img-fluid mb-5"
+        alt="Jornalizacion"
+        width="700"
+        height="700"
+    />
+
+    <div class="p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end text-center">
+    <h4>A continuación se muestran fotos de la página web:</h4>
+    </div>
+    <br/>
+    <img
+        src="../assets/tarea/aula2.jpg"
+        class="img-fluid mb-5"
+        alt="Jornalizacion"
+        width="700"
+        height="700"
+    />
+        <img
+        src="../assets/tarea/3.jpg"
+        class="img-fluid mb-5"
+        alt="Jornalizacion"
+        width="700"
+        height="700"
+    />
+        <img
+        src="../assets/tarea/4.jpg"
+        class="img-fluid mb-5"
+        alt="Jornalizacion"
+        width="700"
+        height="700"
+    />
+        <img
+        src="../assets/tarea/5.jpg"
+        class="img-fluid mb-5"
+        alt="Jornalizacion"
+        width="700"
+        height="700"
+    />
+        <img
+        src="../assets/tarea/6.jpg"
+        class="img-fluid mb-5"
+        alt="Jornalizacion"
+        width="700"
+        height="700"
+    />
+</div>
+</template>
